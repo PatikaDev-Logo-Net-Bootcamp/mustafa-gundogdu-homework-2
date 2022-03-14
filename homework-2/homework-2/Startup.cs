1e4b503b00d38a5904cdd47ddaa9f669f8a1edfa
mustgndgd <mstfgndgd1997@gmail.com>
2022-03-15 01:45:29 +0300

    son

--- homework-2/homework-2/Startup.cs
+++ homework-2/homework-2/Startup.cs
@@ -1,17 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.OpenApi.Models; 
 using homework_2.Middlewares;
+using homework_2.SwaggerHelper;
 
 namespace homework_2
 {
